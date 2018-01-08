b560c8e50e41b9df948e33181345faaf320b3980
Yuya Tabata <yuya1212h4.066@gmail.com>
2018-01-08 09:29:03 +0900

    create phone factory

---
+++ spec/factories/contacts.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :contact do
+    firstname { Faker::Name.first_name }
+    lastname { Faker::Name.last_name }
+    email { Faker::Internet.email }
+
+    # after(:build) do |contact|
+    #   [:home_phone, :work_phone, :mobile_phone].each do |phone|
+    #     contact.phones << FactoryBot.build(:phone,
+    #       phone_type: phone, contact: contact)
+    #   end
+    # end
+  end
+end
